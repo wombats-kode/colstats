52e5f02b7d9289e744457421a18e7eb991acf9ae
wombats-kode <wombat1913@gmail.com>
2024-10-25 11:35:58 +0100

    improved concurrent execution by linking to CPU count

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/wombats-kode/colstats
 
 go 1.23.1
-
-require golang.org/x/tools v0.26.0 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime"
 	"sync"
 )
 
@@ -45,39 +46,50 @@ func run(filenames []string, op string, column int, out io.Writer) error {
 	consolidate := make([]float64, 0)
 
 	// Create the channel to receive results or errors of operations
+	filesCh := make(chan string)
 	resCh := make(chan []float64)
 	errCh := make(chan error)
 	doneCh := make(chan struct{})
 
 	wg := sync.WaitGroup{}
 
+	// Loop through all files sending them through the channel
+	// so each one will be processed when a worker is available.
+	go func() {
+		defer close(filesCh)
+		for _, fname := range filenames {
+			filesCh <- fname
+		}
+	}()
+
 	// Loop through all files and create a goroutine to process
 	// each one concurrently
-	for _, fname := range filenames {
+	for i := 0; i < runtime.NumCPU(); i++ {
 		wg.Add(1)
-		go func(fname string) {
+		go func() {
 			defer wg.Done()
+			for fname := range filesCh {
+				// Open the file for reading
+				f, err := os.Open(fname)
+				if err != nil {
+					errCh <- fmt.Errorf("cannot open file: %w", err)
+					return
+				}
 
-			// Open the file for reading
-			f, err := os.Open(fname)
-			if err != nil {
-				errCh <- fmt.Errorf("cannot open file: %w", err)
-				return
-			}
+				// Parse the CSV into a slice of float64 numbers
+				data, err := csv2float(f, column)
+				if err != nil {
+					errCh <- err
+				}
 
-			// Parse the CSV into a slice of float64 numbers
-			data, err := csv2float(f, column)
-			if err != nil {
-				errCh <- err
+				if err := f.Close(); err != nil {
+					errCh <- err
+				}
+				// If no errors - send the csv2float data into the
+				// results channel.
+				resCh <- data
 			}
-
-			if err := f.Close(); err != nil {
-				errCh <- err
-			}
-			// If no errors - send the csv2float data into the
-			// results channel.
-			resCh <- data
-		}(fname) // End the anonymous function, passing in the variable
+		}() // End the anonymous function
 	}
 
 	// Wait until all files hav been processed
